b57d7aa9f3764ed336b865f28bc0c6cb95769444
YeshwithaBM <yeshugowda477@gmail.com>
2026-08-18 12:08:32 +0530

    calculator

--- projects/project6/calculator.py
+++ projects/project6/calculator.py
@@ -2,37 +2,40 @@ import os
 def calculate(first_element):
     while True:
         choose_operation=input("pick an operation: ")
-        next_number=int(input("enter next number: "))
+        next_number=float(input("enter next number: "))
         if choose_operation=='+':
             result=first_element+next_number
             print(f'{first_element}+{next_number}={result}')
         elif choose_operation=='-':
             result=first_element-next_number
-            print(f'{first_element}+{next_number}={result}')
+            print(f'{first_element}-{next_number}={result}')
         elif choose_operation=='*':
             result=first_element*next_number
-            print(f'{first_element}+{next_number}={result}')
+            print(f'{first_element}*{next_number}={result}')
         elif choose_operation=='/':
             if next_number==0:
                 print("Can't divide by 0")
+                continue
             else:
                 result=first_element/next_number
-                print(f'{first_element}+{next_number}={result}')
+                print(f'{first_element}/{next_number}={result}')
         else:
             print("invalid option")
+            return 
+
         ask_again=input(f"enter 'y' to continue with {result} or 'n' to start new calculation or 'x' to exit: ").lower()
         if ask_again=='y':
             return calculate(result)
         elif ask_again=='n':
             os.system('cls')
-            new_element=int(input("enter a number: "))
+            new_element=float(input("enter a number: "))
             operator=["+","-","/","*"]
             for i in operator:
                 print(i)
             return calculate(new_element)
         else:
             break
-first_element=int(input("enter a number: "))
+first_element=float(input("enter a number: "))
 operator=["+","-","/","*"]
 for i in operator:
     print(i)
